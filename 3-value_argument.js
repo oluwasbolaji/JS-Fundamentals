ce31660654b01e4facec892e2d623877a578dba0
Oluwasola Bolaji <oluwasbolaji1993@gmail.com>
2025-11-07 18:28:20 +0100

    Fix exercise 3: remove length usage

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args.length === 0) {
+if (arg === undefined) {
   console.log("No argument");
 } else {
-  console.log(args[0]);
+  console.log(arg);
 }
